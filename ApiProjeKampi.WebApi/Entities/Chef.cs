d5c592a1bcf8800eaeb543f9fd11b47204949599
Cihan <cihandikkaya09@hotmail.com>
2026-02-18 17:35:48 +0300

    Entity'ler yazıldı.

---
+++ ApiProjeKampi.WebApi/Entities/Chef.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProjeKampi.WebApi.Entities
+{
+    public class Chef
+    {
+        public int ChefID { get; set; }
+        public string ChefName { get; set; }
+        public string ChefTitle { get; set; }
+        public string ChefDescription { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
---
+++ ApiProjeKampi.WebApi/Entities/Reservation.cs
@@ -0,0 +1,15 @@
+﻿namespace ApiProjeKampi.WebApi.Entities
+{
+    public class Reservation
+    {
+        public int ReservationID { get; set; }
+        public string NameSurname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public string ReservationTime { get; set; }
+        public string CountofPeople { get; set; }
+        public string Message { get; set; }
+        public string Status { get; set; }
+    }
+}
